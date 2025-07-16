b54c999282ed181648f71440d50fd728bc3666a2
AmstlerStephan <96417996+AmstlerStephan@users.noreply.github.com>
2025-07-16 19:03:29 +0200

    Add files via upload

---
+++ pipelines/consequmi/src/run_umi-pipeline-nf_poa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+nextflow run genepi/umi-pipeline-nf \
+	-r reference_based_polishing_karst_context_primer -latest \
+	--use_gpu \
+	-c config/sarscov2_poa_context.config \
+	-profile docker \
+	-with-docker quay.io/genepi/umi-pipeline-nf:vtest_karst
