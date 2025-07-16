37db2091e000dd3367e8bccf9c963d89c7225d9f
AmstlerStephan <96417996+AmstlerStephan@users.noreply.github.com>
2025-07-16 19:15:22 +0200

    Add files via upload

---
+++ pipelines/karst/src/run_umi-pipeline-nf_reference_based.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+nextflow run genepi/umi-pipeline-nf \
+	-r reference_based_polishing_karst_context_primer -latest \
+	--reference_based_polishing \
+	--use_gpu \
+	-c config/zymo_operon_context.config \
+	-profile docker \
+	-with-docker quay.io/genepi/umi-pipeline-nf:vtest_karst
